66290c2512dcffff029e7a1868a3fc7c994b7bff
Yes Man <yesman@Yes-Man.local>
2019-12-01 18:40:58 +0300

    Initial commit

---
+++ src/main/java/pavlov/sergey/filebeat/FilebeatApplication.java
@@ -0,0 +1,13 @@
+package pavlov.sergey.filebeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilebeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilebeatApplication.class, args);
+	}
+
+}
